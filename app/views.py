722c6b0aefe339a697043760c743b8ece012bdac
duxianghua <xianghuadu@gmail.com>
2017-06-17 17:03:03 +0800

    test

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import request
 import json
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -12,8 +13,11 @@ def webhook():
     if request.method == 'POST':
         data = request.get_json()
         project_name = data['repository']['name']
-        print project_name
-        print json.dumps(data, sort_keys=True, indent=2)
+        #print project_name
+        #print json.dumps(data, sort_keys=True, indent=2)
+        command = "python /usr/local/bin/deploy-code.py %s pull && python /usr/local/bin/deploy-code.py %s sync" % project_name
+        #os.system(command)
+        print command
         return 'ok'
     else:
         return 'not get method'

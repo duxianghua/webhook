42e0f1a39efd383b4608c7907a2a1fa24de55963
duxianghua <xianghuadu@gmail.com>
2017-06-17 17:25:25 +0800

    v0.1

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 def index():
     return "This webhook page!"
 
+
 @app.route('/project', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
@@ -15,9 +16,9 @@ def webhook():
         project_name = data['repository']['name']
         #print project_name
         #print json.dumps(data, sort_keys=True, indent=2)
-        command = "python /usr/local/bin/deploy-code.py %s pull && python /usr/local/bin/deploy-code.py %s sync" % (project_name,project_name)
-        #os.system(command)
-        print command
+        if project_name and isinstance(project_name, str):
+            command = "python /usr/local/bin/deploy-code.py %s pull && python /usr/local/bin/deploy-code.py %s sync" % (project_name,project_name)
+            os.system(command)
         return 'ok'
     else:
         return 'not get method'
